408efe9362de977a571b47b1d02fe0cb88b36e92
Mutiara <mutiarapandejlaki27@gmail.com>
2024-09-23 00:29:22 +0800

    generate id

--- source/main.js
+++ source/main.js
@@ -18,12 +18,25 @@ const RENDER_EVENT = "render-note";
 function addNote() {
   const title = document.getElementById("title").value;
   const body = document.getElementById("description").value;
+  // const date
 
-  const generateID = generateId();
+  const generateID = generateId("notes");
   const notesData = generateNotesData(generateID, title, body, date, false);
   story.push(notesData);
 
   document.dispatchEvent(new Event(RENDER_EVENT));
   document.getElementById("noteForm").reset();
   saveData();
+}
+
+function generateId(prefix, length = 5) {
+  const number = Math.floor(10000 + Math.random() * 90000);
+  const word = "abcdefghijklmnopqrstuvwxyz";
+  let suffix = "";
+
+  for (let i = 0; i < length; i++) {
+    suffix += word.charAt(Math.floor(Math.random() * word.length));
+  }
+
+  return `${prefix}-${number}-${suffix}`;
 }
