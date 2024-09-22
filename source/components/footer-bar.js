b8d155b03d36022eedc4d24c11d704369dd39f96
Mutiara <mutiarapandejlaki27@gmail.com>
2024-09-22 15:58:11 +0800

    tmbh components (header & footer)

---
+++ source/components/footer-bar.js
@@ -0,0 +1,57 @@
+class FooterBar extends HTMLElement {
+  _shadowRoot = null;
+  _style = null;
+
+  constructor() {
+    super();
+
+    this._shadowRoot = this.attachShadow({ mode: "open" });
+    this._style = document.createElement("style");
+  }
+
+  _updateStyle() {
+    this._style.textContent = `
+      :host {
+        display: block;
+      }
+
+      div {
+        height: 8.5vh;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: white;
+
+        margin: 0;
+        padding: 0;
+      }
+
+      .sign {
+        text-align: center;
+      }
+    `;
+  }
+
+  _emptyContent() {
+    this._shadowRoot.innerHTML = "";
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this._emptyContent();
+    this._updateStyle();
+
+    this._shadowRoot.appendChild(this._style);
+    this._shadowRoot.innerHTML += `
+      <div>
+        <p class='sign'>Mutii &copy; 2024</p>
+      </div>
+    `;
+  }
+}
+
+customElements.define("footer-bar", FooterBar);
---
+++ source/components/index.js
@@ -0,0 +1,2 @@
+import "./header-bar.js";
+import "./footer-bar.js";
