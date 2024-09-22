e3a5ecada0416999de4f77c5a5192fe38efb02b4
Mutiara <mutiarapandejlaki27@gmail.com>
2024-09-22 23:21:14 +0800

    tmbh function simpan data dalam DOM

--- source/main.js
+++ source/main.js
@@ -1,1 +1,12 @@
 import "./components/index.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const submitNotes = document.getElementById("noteForm");
+  submitNotes.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addNote();
+  });
+  if (isstorageExist()) {
+    loadDataFromStorage();
+  }
+});
