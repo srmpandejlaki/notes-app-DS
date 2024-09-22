b7620f4bd23364c4e41416cd286ca9ff3d81ad19
Mutiara <mutiarapandejlaki27@gmail.com>
2024-09-23 00:10:14 +0800

    tmbh fungsi addNote (belum fix)

--- source/main.js
+++ source/main.js
@@ -10,3 +10,20 @@ document.addEventListener("DOMContentLoaded", function () {
     loadDataFromStorage();
   }
 });
+
+let story = [];
+const RENDER_EVENT = "render-note";
+
+// tambah note baru
+function addNote() {
+  const title = document.getElementById("title").value;
+  const body = document.getElementById("description").value;
+
+  const generateID = generateId();
+  const notesData = generateNotesData(generateID, title, body, date, false);
+  story.push(notesData);
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  document.getElementById("noteForm").reset();
+  saveData();
+}
